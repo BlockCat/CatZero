59980ff7efabff14c377b8af2135d4e09e723e87
BlockCat <zinoonomiwo@gmail.com>
2021-11-26 23:33:06 +0100

    Restructure

--- tensorboard.bat
+++
@@ -1,8 +0,0 @@
-@Echo Off
-call conda activate tensorflow
-
-call set PYTHONHOME=C:\tools\miniconda3\envs\tensorflow
-
-call tensorboard.exe --logdir tictactoe/learn/data/logs/fit
-
-pause
--- tictactoe.bat
+++
@@ -1,8 +0,0 @@
-@Echo Off
-call conda activate tensorflow
-
-call set PYTHONHOME=C:\tools\miniconda3\envs\tensorflow
-call cd tictactoe/learn
-call cargo run --example %1 --release
-
-pause
